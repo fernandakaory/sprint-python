0ceb024cb8f371c1ff6ce8a742f5ac40f83813bc
fernandakaory <rm551104@fiap.com.br>
2023-10-06 11:32:22 -0300

    adicionando arquivos de função

--- sprint3.py
+++ sprint3.py
@@ -1,66 +1,38 @@
 import re
+from funcoes import validaNome, validaEmail, validaSenha, recebeNota
+
 continua = "sim"
 msgFinal = []
 avaliados = []
 
-def validaNome(nome):
-    erro=""
-    if re.search("\d",nome) or nome == '':
-        erro = "Campo obrigatório. Por favor, insira um nome válido, sem números."
-    return erro
-
-def validaEmail(email):
-    erro=""
-    if not re.search("\w+@\w+\.\w+", email) or email == '':
-        erro = "Campo obrigatório. Por favor, insira um e-mail válido."
-    return erro
-
-def validaSenha(senha):
-    erro=""
-    if not re.search("\d{6}",senha) or len(senha) > 6 or senha == '':
-        erro = "Campo obrigatório. Por favor, insira uma senha válida de 6 dígitos."
-    return erro
-
-def recebeNota(avaliacao):
-    erro = ""
-    print(f"\nEscolha uma nota de 1 a 5 para avaliar {avaliacao}, sendo:\n1 - Péssima\n2 - Ruim\n3 - Mediana\n4 - Boa\n5- Excelente")
-    nota= int(input())
-    if nota>0 and nota <= 3:
-        print("\nSentimos muito por isso. Por favor, conte-nos o motivo dessa nota.")
-        motivo = input()
-        print("\nObrigado pelo feedback!")
-        msgFinal.append(f"\nVocê avaliou nota {nota} para {avaliacao}.\nMotivo: {motivo}")
-    elif nota>=3 and nota <=5:
-        print("Ficamos felizes com isso. Obrigado pelo feedback!")
-        msgFinal.append(f"\nVocê avaliou nota {nota} para {avaliacao}.")
-    else:
-        erro= "Por favor, digite um número válido"
-        return erro
-
 try:
-    #área de cadastro e validação 
-    print("CADASTRO")
-    
-    nome = input("Informe seu nome: ")
-    erro = validaNome(nome)
-    if erro:
-        raise ValueError(erro)
-    
-    email = input("Informe seu e-mail: ")
-    erro = validaEmail(email)
-    if erro:
-        raise ValueError(erro)
-    
-    senha = input("Crie uma senha de 6 dígitos: ")
-    erro = validaSenha(senha)
-    if erro:
-        raise ValueError(erro)
+    cadastro = input("Você já possui cadastro? (Digite 'sim' ou 'não')").lower()
 
-    infoCliente = {
-        "nome": nome,
-        "email": email,
-        "senha": senha
-    }
+    if cadastro == "não":
+        #área de cadastro e validação 
+        print("CADASTRO")
+        
+        nome = input("Informe seu nome: ")
+        erro = validaNome(nome)
+        if erro:
+            raise ValueError(erro)
+        
+        email = input("Informe seu e-mail: ")
+        erro = validaEmail(email)
+        if erro:
+            raise ValueError(erro)
+        
+        senha = input("Crie uma senha de 6 dígitos: ")
+        erro = validaSenha(senha)
+        if erro:
+            raise ValueError(erro)
+
+        infoCliente = {
+            "nome": nome,
+            "email": email,
+            "senha": senha
+        }
+        
 
     print("\nVocê está no ônibus ST23. Rota Vila Mariana.")
     while continua.lower() == "sim":
@@ -70,22 +42,22 @@ try:
 # menu de escolhas
         if escolha not in avaliados:
             if escolha == "1":
-                erro = recebeNota("condição do ônibus")
+                erro = recebeNota("condição do ônibus",msgFinal)
                 if erro:
                     raise ValueError
 
             elif escolha == "2":
-                erro = recebeNota("rota atual")
+                erro = recebeNota("rota atual",msgFinal)
                 if erro:
                     raise ValueError
 
             elif escolha == "3":
-                erro = recebeNota("experiência usando o aplicativo")
+                erro = recebeNota("experiência usando o aplicativo",msgFinal)
                 if erro:
                     raise ValueError
 
             elif escolha == "4":
-                erro = recebeNota("pontualidade do ônibus")
+                erro = recebeNota("pontualidade do ônibus",msgFinal)
                 if erro:
                     raise ValueError
 
